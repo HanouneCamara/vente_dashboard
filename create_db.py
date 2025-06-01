23000153c0de46c9ad58aa553b0171d37e3fd83d
HanouneCamara <Alicmr13@gmail.com>
2025-06-01 12:40:47 +0100

    create_db

--- create_db.py
+++ create_db.py
@@ -1,3 +1,26 @@
 import sqlite3
 import os
 
+# Vérifier si le dossier "db" existe
+if not os.path.exists("db"):
+    os.makedirs("db")
+    
+# Connexion à la bdd
+conn = sqlite3.connect("db/ventes.db")
+cursor = conn.cursor()
+
+# Création de la table ventes
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS ventes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT,
+    produit TEXT,
+    quantite INTEGER,
+    prix_unitaire REAL,
+    client TEXT
+)   
+""")
+conn.commit()
+conn.close()
+
+print("Base de données creer")
